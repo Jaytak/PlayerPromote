e733ce6c2704a03ffd1c8b338e7e43bd1ec51724
Jay <78488517+Jaytak@users.noreply.github.com>
2024-10-11 17:32:38 +1300

    0.1.19

--- src/main/java/com/jaytak/playerPromote/PlayerPromote.java
+++ src/main/java/com/jaytak/playerPromote/PlayerPromote.java
@@ -8,16 +8,29 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PlayerPromote extends JavaPlugin {
+import java.util.Objects;
+
+public final class PlayerPromote extends JavaPlugin implements Listener {
     private LuckPerms luckPerms;
     private String playerTitle = "§2§l[§r§2Player Promote§2§l]§r ";
+    private String newPlayerMessage = "";
+    private String promoteMessage = "";
+    private String alreadyPromotedMessage = "";
+    private String initalGroup = "default";
+    private String promoteToGroup = "group.player";
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
+        loadConfig();
         this.getCommand("agree").setExecutor(new AgreeCommand());
+
         luckPerms = getServer().getServicesManager().load(LuckPerms.class);
         if (luckPerms == null) {
             getLogger().severe(playerTitle + "LuckPerms not found! This plugin requires LuckPerms.");
@@ -35,6 +48,23 @@ public final class PlayerPromote extends JavaPlugin {
 
     }
 
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (!event.getPlayer().hasPlayedBefore() && !Objects.equals(newPlayerMessage, "DISABLED")) {
+            event.getPlayer().sendMessage(newPlayerMessage);
+        }
+    }
+
+    private void loadConfig(){
+        playerTitle = getConfig().getString("PlayerTitle", "§2§l[§r§2Player Promote§2§l]§r ");
+        newPlayerMessage = getConfig().getString("NewPlayerMessage", "This is the first time you have joined. Please read the rules at spawn and you can promote yourself to player once you have agreed to the rules.");
+        promoteMessage = getConfig().getString("PromoteMessage", "You have been promoted to the Player group! GLHF!");
+        alreadyPromotedMessage = getConfig().getString("AlreadyPromotedMessage","You are already in the group Player, or higher.");
+        initalGroup = getConfig().getString("DefaultGroup", "default");
+        promoteToGroup = getConfig().getString("PromoteToGroup", "group.player");
+    }
+
+
     public class AgreeCommand implements CommandExecutor {
         @Override
         public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -45,29 +75,28 @@ public final class PlayerPromote extends JavaPlugin {
 
             Player player = (Player) sender;
 
-            if (!player.hasPermission("agreeplugin.use")) {
-                player.sendMessage(playerTitle + "You do not have permission to use this command.");
-                return true;
+            if (command.getName().equalsIgnoreCase("agree")) {
+                if (!player.hasPermission("agreeplugin.use")) {
+                    player.sendMessage(playerTitle + "You do not have permission to use this command.");
+                    return true;
+                }
+
+                User user = luckPerms.getUserManager().getUser(player.getUniqueId());
+
+                if (user == null) {
+                    player.sendMessage(playerTitle + "Error fetching your data. Try again later.");
+                    return true;
+                }
+
+                if (user.getPrimaryGroup().equals(initalGroup)) {
+                    user.data().add(Node.builder(promoteToGroup).build());
+                    luckPerms.getUserManager().saveUser(user);
+                    player.sendMessage(playerTitle + promoteMessage);
+                }
+                else {
+                    player.sendMessage(playerTitle + alreadyPromotedMessage);
+                }
             }
-
-            User user = luckPerms.getUserManager().getUser(player.getUniqueId());
-
-            if (user == null) {
-                player.sendMessage(playerTitle + "Error fetching your data. Try again later.");
-                return true;
-            }
-
-            if (user.getPrimaryGroup().equals("default")) {
-
-                user.data().add(Node.builder("group.player").build());
-
-                luckPerms.getUserManager().saveUser(user);
-
-                player.sendMessage(playerTitle + "You have been promoted to the Player group! GLHF!");
-            } else {
-                player.sendMessage(playerTitle + "You are already in the group Player, or higher.");
-            }
-
             return true;
         }
     }
